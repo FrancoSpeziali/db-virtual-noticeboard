9412383e739499b226ed606756317363dee66a9c
Franco Speziali <60964014+FrancoSpeziali@users.noreply.github.com>
2022-04-22 16:44:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const path = require("path");
+import express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 const port = 3001;
 const clientUrl = "http://localhost:3001";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // !! Your middleware should not go below this line !!
+
 // Serve frontend client/build folder
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
